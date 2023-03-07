86d5a70027b09a2b7d896060b3745420c52693e9
Yury.Fedoseev <Yri95@inbox.ru>
2023-03-07 17:11:45 +0300

    DragAndDrops2

--- src/test/java/guru/qa/TestDragAndDrops.java
+++ src/test/java/guru/qa/TestDragAndDrops.java
@@ -1,2 +1,45 @@
-package guru.qa;public class TestDragAndDrops {
+package guru.qa;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.Configuration.holdBrowserOpen;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestDragAndDrops {
+
+    private int xOffset = 150;
+    private int yOffset = 2;
+
+
+    @BeforeAll
+    static void beforeAll() {
+        browserSize = "1220x880";
+        holdBrowserOpen = true;
+    }
+
+    @Test
+    public void dragAndDrops() {
+        //Работает
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    public void dragAndDropsWithAction() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        //SelenideElement element = $("#column-a");
+        //actions().dragAndDropBy(element, xOffset, yOffset).perform();
+        SelenideElement elementA = $("#column-a");
+        SelenideElement elementB = $("#column-b");
+        //actions().contextClick(elementA).moveToElement(elementB);
+        actions().dragAndDrop(elementA,elementB);
+        //$("#column-b").shouldHave(text("A"));
+
+
+    }
 }
--- src/test/java/guru/qa/TestSearchWithHover.java
+++ src/test/java/guru/qa/TestSearchWithHover.java
@@ -1,5 +1,36 @@
 package guru.qa;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.commands.Hover;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class TestSearchWithHover {
+    @BeforeAll
+    static void beforeAll()
+    {
+    browserSize = "1220x880";
+    holdBrowserOpen = true;
+    }
+
+
+    @Test
+    public void searchElementsOnGit(){
+        open("https://github.com/");
+        $$(".HeaderMenu-link").findBy(text("Solutions")).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+
+        $("div.enterprise-hero").shouldHave(text("Build like the best"));
+
+
+
+
+    }
 
 }
